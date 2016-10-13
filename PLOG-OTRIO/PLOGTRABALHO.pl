a8e401c80ee630ee001ed09ca7b2f895bea922fa
Nuno Silva <nuno_nfss96@hotmail.com>
2016-10-13 19:42:32 +0100

    EZ PZ

--- PLOG-OTRIO/PLOGTRABALHO.pl
+++ PLOG-OTRIO/PLOGTRABALHO.pl
@@ -6,28 +6,6 @@ board([
 
 
 
-/*
-board([
-[a,a,a],[b,b,b],[a,a,a],
-[c,c,c],[d,d,d],[c,c,c],
-[a,a,a],[b,b,b],[a,a,a]
-]).
-*/
-/*
-%print the board
-print_board([]).
-print_board([Lin|Rest]):-
-	print_line(Lin),
-	print_board(Rest).
-
-%Print 1 line of the board
-print_line([]):-write(' FIN ').
-print_line([Elem|Rest]):-
-	translate(Elem, Plem),
-	write(Plem),
-	print_line(Rest).
-*/
-
 %print the board
 print_board([]).
 print_board([Lin|Rest]):-
@@ -39,7 +17,8 @@ print_line([]):- nl.
 print_line([Elem|Rest]):-
 	printaux(Elem),
 	print_line(Rest).
-	
+
+%print the set of pieces in a position
 printaux([]) :- write('   ').
 printaux([X|Rest]):-
 	translate(X, Plem),
@@ -48,20 +27,15 @@ printaux([X|Rest]):-
 
 
 
-% Translate the board to the displaying simbols
+% Translate the board to the displaying symbols
 translate(a,'0').
 translate(b,'1').
 translate(c,'2').
 translate(d,'3').
 
-linha1G([
-[a,a,a],[b,b,b],[c,c,c]
-]).
-
 % Display the board
 display :- board(X), print_board(X).
 
-teste :- linha1G(X), print_board(X).
 
 
 
