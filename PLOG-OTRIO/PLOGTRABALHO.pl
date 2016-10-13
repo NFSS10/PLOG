a46f4a9a48ae805b8137e1fd8696abdabd29ad7e
Nuno Silva <nuno_nfss96@hotmail.com>
2016-10-13 18:26:28 +0100

    Teste de print

--- PLOG-OTRIO/PLOGTRABALHO.pl
+++ PLOG-OTRIO/PLOGTRABALHO.pl
@@ -1,15 +1,22 @@
-T=([[B,B,B],[N,N,N],[N,R,N]],
-[[N,N,N],[N,V,N],[P,P,G]],
-[[P,N,N],[R,B,R],[G,G,G]]).
+board([[a,a,a],[b,b,b]]).
 
+%print the board
 print_board([]).
 print_board([Lin|Rest]):-
 	nl,
-	print_line(Lin),print_board(Rest).
+	print_line(Lin),
+	print_board(Rest).
 
+%Print 1 line of the board
 print_line([]).
 print_line([Elem|Rest]):-
-translate(Elem,Plem),
-write(Plem),
-print_line(Rest).
+	translate(Elem, Plem),
+	write(Plem),
+	print_line(Rest).
 
+% Translate the board to the displaying simbols
+translate(a,'0').
+translate(b,'1').
+
+% Display the board
+display :- board(X), print_board(X).
