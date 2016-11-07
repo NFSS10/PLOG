a8c3d3a790fd9352a70ccf693714e56c3eeec94d
up201404022 <neoliator25@gmail.com>
2016-11-07 22:54:11 +0000

    jogada jogador 1 almost done

--- PLOG-OTRIO/interface.pl
+++ PLOG-OTRIO/interface.pl
@@ -1,31 +1,5 @@
 :- consult(draw).
 :- consult(engine).
 
-%posicoes validas
-posicaoValidaLin(1).
-posicaoValidaLin(2).
-posicaoValidaLin(3).
-posicaoValidaCol(a).
-posicaoValidaCol(b).
-posicaoValidaCol(c).
 
-%Pede ao utilizador as coordenadas e recolhe as mesmas
-getCoord(Lin, Col) :- write('Insira a coordenada desejada'), nl,
-			write('Linha ("1" a "3"): '),
-			read(Lin),
-			write('Coluna ("A" a "C"): '),
-			read(Col),
-			nl.
-
-%Se a posicao e valida //TODO trocar testeCorreto
-posicaoValida(Lin, Col) :- posicaoValidaLin(Lin), posicaoValidaCol(Col), !, testeCorreto.
-%Se Invalida
-posicaoValida(Lin, Col) :- msgCoordInvalida, readCoords.
-msgCoordInvalida :- nl, nl, write('--- Coordenadas Incorretas, Insire corretamente as coordenadas ---'), nl, nl.
-
-%Pede as coordenadas ao utlizador
-readCoords :- getCoord(Lin, Col), posicaoValida(Lin, Col).
-
-
-testeCorreto :- nl, write('CORRETO'), nl.
-
+%%getCoord(Lin, Col)
--- PLOG-OTRIO/main.pl
+++ PLOG-OTRIO/main.pl
@@ -52,3 +52,112 @@ teste2 :-	write('LER'),nl,
 			nl, write(B),
 			nl.
 
+%%%%%%%%%%%%%%%%%%%%%FIZ DAQUI PARA BAIXO
+
+%Da replace na lista dado o indx(I) e o valor pretendido(X) devolva a lista modificada
+
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > 0,	
+							  I1 is I-1,
+							  replace(T, I1, X, R).
+
+
+
+%Coloca peca pequena no tabuleiro
+							  
+colocarpequena(X,Y,P):-                	board(ListaSai),
+										nth0(Y,ListaSai,Elemento,Resto), 
+										nth0(2,Elemento,Elemento2,Resto2),
+										nth0(X,Elemento2,Elemento3,Resto3),
+										Elemento3=n1,!,
+										replace(Elemento2, X, P, Lis),
+										replace(Elemento,2,Lis,List),
+										replace(ListaSai,Y,List,ListaSai2),
+										asserta(board(ListaSai2)),
+										display.							  
+							  
+colocarpequena(X,Y,P):- nl, write('Posicao ocupada!'),fail.
+							  
+%Coloca peca media no tabuleiro
+							  
+colocarmedia(X,Y,P):-                	board(ListaSai),
+										nth0(Y,ListaSai,Elemento,Resto), 
+										nth0(1,Elemento,Elemento2,Resto2),
+										nth0(X,Elemento2,Elemento3,Resto3),
+										Elemento3=n2,!,
+										replace(Elemento2, X, P, Lis),
+										replace(Elemento,1,Lis,List),
+										replace(ListaSai,Y,List,ListaSai2),
+										asserta(board(ListaSai2)),
+										display.
+
+colocarmedia(X,Y,P):- nl, write('Posicao ocupada!'),fail.
+
+%Coloca peca grande no tabuleiro
+
+colocargrande(X,Y,P):-					board(ListaSai),
+										nth0(Y,ListaSai,Elemento,Resto), 
+										nth0(0,Elemento,Elemento2,Resto2),
+										nth0(X,Elemento2,Elemento3,Resto3),
+										Elemento3=n3,!,
+										replace(Elemento2, X, P, Lis),
+										replace(Elemento,0,Lis,List),
+										replace(ListaSai,Y,List,ListaSai2),
+										asserta(board(ListaSai2)),
+										display.
+										
+colocargrande(X,Y,P):- nl, write('Posicao ocupada!'),fail.
+
+getcolor(Peca,Cor):- atom_chars(Peca,Char),
+					nth0(0,Char,Cor,Resto2).
+
+getsize(Peca,Tamanho):- atom_chars(Peca,Char),
+					nth0(1,Char,Tamanho,Resto2).
+
+
+%Faz jogada do jogador X TODO-> substituir a peca no set
+
+jogadajogador1(X,Y,Peca):-getsize(Peca,Size),
+						 Size='1',!,
+						 p1Set(Set),
+						 verificapecapequena(Set,Peca),
+						 colocarpequena(X,Y,Peca).
+						 
+jogadajogador1(X,Y,Peca):-getsize(Peca,Size),
+						 Size='2',!,
+						 p1Set(Set),
+						 verificapecamedia(Set,Peca),
+						 colocarmedia(X,Y,Peca).
+						 
+jogadajogador1(X,Y,Peca):-getsize(Peca,Size),
+						 Size='3',!,
+						 p1Set(Set),
+						 verificapecagrande(Set,Peca),
+						 colocargrande(X,Y,Peca).
+						 
+
+
+%Verifica peca
+verificapecagrande(Set,Peca):- 
+							   nth0(0,Set,Lista,Resto2),
+							   nth0(0,Lista,Lista2,Resto3),
+							   member(Peca,Lista2),!.
+							   
+verificapecagrande(Set,Peca):- nl, write('Peca indisponivel para jogar'),fail.
+							   
+							   
+
+
+verificapecamedia(Set,Peca):-
+							   nth0(0,Set,Lista,Resto2),
+							   nth0(1,Lista,Lista2,Resto3),
+							   member(Peca,Lista2),!.
+							   
+verificapecamedia(Set,Peca):- nl, write('Peca indisponivel para jogar'),fail.
+
+
+verificapecapequena(Set,Peca):- nth0(0,Set,Lista,Resto2),
+							   nth0(2,Lista,Lista2,Resto3),
+							   member(Peca,Lista2),!.
+							   
+verificapecapequena(Set,Peca):- nl, write('Peca indisponivel para jogar'),fail.
