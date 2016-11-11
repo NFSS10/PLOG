adc622efd3f095026517c7220b25e797c5cb9d3b
Nuno Silva <nuno_nfss96@hotmail.com>
2016-11-11 22:46:01 +0000

    fix merge

--- PLOG-OTRIO/interface.pl
+++ PLOG-OTRIO/interface.pl
@@ -5,6 +5,54 @@
 posicaoValidaLin(0).
 posicaoValidaLin(1).
 posicaoValidaLin(2).
+posicaoValidaCol(a).
+posicaoValidaCol(b).
+posicaoValidaCol(c).
+
+%Pede ao utilizador as coordenadas e recolhe as mesmas
+getCoord(Lin, Col) :- write('Insira a coordenada desejada'), nl,
+			write('Linha ("0" a "2"): '),
+			read(Lin),
+			write('Coluna ("A" a "C"): '),
+			read(Col),
+			nl.
+
+%Se a posicao e valida //TODO trocar testeCorreto
+posicaoValida(Lin, Col) :- posicaoValidaLin(Lin), posicaoValidaCol(Col), !, testeCorreto.
+%Se Invalida
+posicaoValida(Lin, Col) :- msgCoordInvalida, readCoords.
+msgCoordInvalida :- nl, nl, write('--- Coordenadas Incorretas, Insire corretamente as coordenadas ---'), nl, nl.
+
+%Pede as coordenadas ao utlizador
+readCoords :- getCoord(Lin, Col), posicaoValida(Lin, Col).
+
+
+testeCorreto :- nl, write('CORRETO'), nl.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+paulo
+:- consult(draw).
+:- consult(engine).
+
+%posicoes validas
+posicaoValidaLin(0).
+posicaoValidaLin(1).
+posicaoValidaLin(2).
 posicaoValidaCol(0).
 posicaoValidaCol(1).
 posicaoValidaCol(2).
@@ -37,3 +85,4 @@ readCoords(X1,Y1) :- getCoord(X1, Y1),!, posicaoValida(X, Y).
 
 testeCorreto :- nl, write('CORRETO'), nl.
 
+*/
