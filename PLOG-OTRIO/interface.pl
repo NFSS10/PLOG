51ca9183fb081f21017e62e1b284a31f5c6208f3
up201404022 <neoliator25@gmail.com>
2016-11-12 00:09:33 +0000

    Add files via upload

--- PLOG-OTRIO/interface.pl
+++ PLOG-OTRIO/interface.pl
@@ -1,76 +1,6 @@
 :- consult(draw).
 :- consult(engine).
 
-%posicoes validas
-posicaoValidaLin(0).
-posicaoValidaLin(1).
-posicaoValidaLin(2).
-posicaoValidaCol(a).
-posicaoValidaCol(b).
-posicaoValidaCol(c).
-
-%Pede ao utilizador as coordenadas e recolhe as mesmas
-getCoord(Lin, Col) :- write('Insira a coordenada desejada'), nl,
-			write('Linha ("0" a "2"): '),
-			read(Lin),
-			write('Coluna ("A" a "C"): '),
-			read(Col),
-			nl.
-
-%Se a posicao e valida //TODO trocar testeCorreto
-posicaoValida(Lin, Col) :- posicaoValidaLin(Lin), posicaoValidaCol(Col), !, testeCorreto.
-%Se Invalida
-posicaoValida(Lin, Col) :- msgCoordInvalida, readCoords.
-msgCoordInvalida :- nl, nl, write('--- Coordenadas Incorretas, Insire corretamente as coordenadas ---'), nl, nl.
-
-%Pede as coordenadas ao utlizador
-readCoords :- getCoord(Lin, Col), posicaoValida(Lin, Col).
-
-
-testeCorreto :- nl, write('CORRETO'), nl.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-paulo
-:- consult(draw).
-:- consult(engine).
-
-%posicoes validas
-posicaoValidaLin(0).
-posicaoValidaLin(1).
-posicaoValidaLin(2).
-posicaoValidaCol(0).
-posicaoValidaCol(1).
-posicaoValidaCol(2).
-
-%Pede ao utilizador as coordenadas e recolhe as mesmas
-getCoord(X, Y,Peca) :- write('Insira a coordenada desejada'), nl,
-			write('X ("0" a "2"): '),
-			read(X),
-			posicaoValidaLin(X),
-			write('Y ("0" a "2"): '),
-			read(Y),
-			posicaoValidaCol(Y),
-			write('Ecolha peca (corTamanho, exemplo r3 para peça vermelha e grande)'),
-			read(Peca),
-			nl.
-
-getCoord(X, Y) :- write('Posicao invalida!'),nl,getCoord(X, Y).
-
 
 
 %Se a posicao e valida //TODO trocar testeCorreto
@@ -85,4 +15,3 @@ readCoords(X1,Y1) :- getCoord(X1, Y1),!, posicaoValida(X, Y).
 
 testeCorreto :- nl, write('CORRETO'), nl.
 
-*/
