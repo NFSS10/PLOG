f997997d012a8fdd47e0373dc1434d4a593bef58
Nuno Silva <nuno_nfss96@hotmail.com>
2016-12-20 10:16:25 +0000

    print fix

--- T2/print.pl
+++ T2/print.pl
@@ -12,52 +12,59 @@
 %	[]
 % [][][][]
 % 	[]
-display_cube(Cube, N)	:-	display_cubeAux(Cube, N),!.
+display_cube(Cube, N)	:-	nl, nl, display_cubeAux(Cube, N),!.
 
 
 
 %Chama o necessario para desenhar por completo o cubo
-display_cubeAux(Cube, N)	:-	LimMinF1 is (4*N*N),
-							LimMaxF1 is LimMinF1 + (2*N*N) - N - 1,
-							LimMinF2 is (4*N*N) + N,
-							LimMaxF2 is LimMinF2 + (2*N*N) - N - 1,
-							display_cubeF1(Cube, N,N, LimMinF1, LimMaxF1, 0, 0, N),
+display_cubeAux(Cube, N)	:-	NIF1 is (4*N*N),
+							NIF6 is (4*N*N) + N,
+							write('  '),display_cubeF1espaco(N),display_cubetraco(N),nl,
+							display_cubeF1(Cube, N,N, NIF1, 0, 0, N),
+							write(' ###'),display_cubetraco(4*N),nl,
 							display_cubeFC(Cube, N,N, 0, 0, 0, N),
-							display_cubeF1(Cube, N,N, LimMinF2, LimMaxF2, 0, 0, N).
+							write(' ###'),display_cubetraco(4*N),nl,
+							display_cubeF1(Cube, N,N, NIF6, 0, 0, N),
+							write('  '),display_cubeF1espaco(N),display_cubetraco(N),nl.
 
 
 
 %Desenha os n espacos para centrar uma linha
 display_cubeF1espaco(0).
 display_cubeF1espaco(N)	:-	N1 is N - 1,
-							write('    '),
+							write('   '),
 							display_cubeF1espaco(N1).
 
-
+%Desenha os n tracos
+display_cubetraco(0).
+display_cubetraco(N)	:-	N1 is N - 1,
+							write('###'),
+							display_cubetraco(N1).
+							
 
 						
 						
-%Display do topo
-%	[]
-% --------
-% 	--		
-display_cubeF1(_, 0, _, _, _, _, _, _).
-display_cubeF1(Cube, NumLinhas, NumColunas, LimMin, LimMax, NL, NC, N)	:-	NumLinhas1 is NumLinhas - 1,
+%Display do topo	e	%Display da base			
+%	[]					%	--
+% --------				% --------
+% 	--					% 	[]	
+display_cubeF1(_, 0, _, _, _, _, _).
+display_cubeF1(Cube, NumLinhas, NumColunas, LimMin, NL, NC, N)	:-	NumLinhas1 is NumLinhas - 1,
 							display_cubeF1espaco(NumColunas),
-							display_cubeF1linha(Cube, NumLinhas, NumColunas, LimMin, LimMax, NL, NC, N),
+							write(' |'),display_cubeF1linha(Cube, NumLinhas, NumColunas, LimMin, NL, NC, N),write('|'),
 							nl,nl,
 							NL1 is NL + 1,
-							display_cubeF1(Cube, NumLinhas1, NumColunas, LimMin, LimMax, NL1, 0, N).
+							display_cubeF1(Cube, NumLinhas1, NumColunas, LimMin, NL1, 0, N).
 
 							
 %Linha do topo		
-display_cubeF1linha(_, _, 0, _, _, _, _, _).
-display_cubeF1linha(Cube, NLinhas, NumColunas, LimMin, LimMax, NL, NC, N)	:-	NumColunas1 is NumColunas - 1,
+display_cubeF1linha(_, _, 0, _, _, _, _).
+display_cubeF1linha(Cube, NLinhas, NumColunas, LimMin, NL, NC, N)	:-	NumColunas1 is NumColunas - 1,
 																Pos is LimMin + (2*N*NL) + NC,
 																nth0(Pos,Cube,Peca),
 																write(' '), write(Peca),write(' '),
 																NC1 is NC + 1, 
-																display_cubeF1linha(Cube, NLinhas, NumColunas1, LimMin, LimMax, NL, NC1, N).
+																display_cubeF1linha(Cube, NLinhas, NumColunas1, LimMin, NL, NC1, N).
 				
 				
 				
@@ -69,10 +76,15 @@ display_cubeF1linha(Cube, NLinhas, NumColunas, LimMin, LimMax, NL, NC, N)	:-	Num
 % 	--					
 display_cubeFC(_, 0, _, _, _, _, _).
 display_cubeFC(Cube, NumLinhas, NumColunas, NLInicial, NL, NC, N)	:-	NumLinhas1 is NumLinhas - 1,
+							write('|'),
 							display_cubeFacelinha(Cube, NumLinhas, NumColunas, 0, NL, NC, N),
+							write('|'),
 							display_cubeFacelinha(Cube, NumLinhas, NumColunas, N, NL, NC, N),
+							write('|'),
 							display_cubeFacelinha(Cube, NumLinhas, NumColunas, (2*N), NL, NC, N),
+							write('|'),
 							display_cubeFacelinha(Cube, NumLinhas, NumColunas, (3*N), NL, NC, N),
+							write('|'),
 							nl,nl,
 							NL1 is NL + 1,
 							display_cubeFC(Cube, NumLinhas1, NumColunas, NLInicial, NL1, NC, N).
@@ -92,26 +104,6 @@ display_cubeFacelinha(Cube, NLinhas, NumColunas, NLInicial, NL, NC, N)	:-	NumCol
 
 
 																
-%Display da base
-%	--
-% --------
-% 	[]			
-display_cubeBase(_, 0, _, _, _, _, _, _).
-display_cubeBase(Cube, NumLinhas, NumColunas, LimMin, LimMax, NL, NC, N)	:-	NumLinhas1 is NumLinhas - 1,
-							display_cubeF1espaco(NumColunas),
-							display_cubeBaselinha(Cube, NumLinhas, NumColunas, LimMin, LimMax, NL, NC, N),
-							nl,nl,
-							NL1 is NL + 1,
-							display_cubeBase(Cube, NumLinhas1, NumColunas, LimMin, LimMax, NL1, 0, N).
 
-							
-							
-display_cubeBaselinha(_, _, 0, _, _, _, _, _).
-display_cubeBaselinha(Cube, NLinhas, NumColunas, LimMin, LimMax, NL, NC, N)	:-	NumColunas1 is NumColunas - 1,
-																Pos is LimMin + (2*N*NL) + NC,
-																nth0(Pos,Cube,Peca),
-																write(' '), write(Peca),write(' '),
-																NC1 is NC + 1, 
-																display_cubeBaselinha(Cube, NLinhas, NumColunas1, LimMin, LimMax, NL, NC1, N).
-							
+
 							
